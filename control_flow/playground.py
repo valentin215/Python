4fd4dde86926b79dfd6ccf32f2ac08870c387167
Valentin Birault <valentin@amenitiz.io>
2020-05-31 21:25:06 +0200

    modif

--- control_flow/playground.py
+++ control_flow/playground.py
@@ -11,7 +11,9 @@ def queue(queuers,pos):
     frontOfLine = queuers[:pos+1]
     backOfLine = queuers[pos+1:]
     frontOfLine = [min(x, friendWait) for x in frontOfLine]
+    print(frontOfLine)
     backOfLine = [min(x, friendWait-1) for x in backOfLine]
+    print(backOfLine)
     return sum(frontOfLine) + sum(backOfLine)
 
-queue([1, 2, 3, 4, 5, 6], 3)
+queue([56, 2, 3, 4, 5, 6], 3)
