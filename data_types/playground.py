fc6df177dd0819cb61a8c8e06ee312012062ad33
Valentin Birault <valentin@amenitiz.io>
2020-05-31 15:45:12 +0200

    matrix list

--- data_types/playground.py
+++ data_types/playground.py
@@ -35,6 +35,47 @@ test_tuple = ('John', 'Peter', 'Val')
 result = ', '.join(test_tuple)
 print(result)
 
-formating_variable = 'Hello'
-# sentence = f'We are in {formating_variable}'
-print(f'We are in {formating_variable}')
+formating_variable = 2020
+sentence = f'We are in {formating_variable}'
+print(f'We are in {formating_variable}') 
+print(sentence)
+
+# .format 
+sentence_test = '{}, were are in {}'.format('Hello', 2020)
+print(sentence_test)
+
+""" List type
+"""
+list_test = [1, 2, 3]
+list_test.append(4)
+print(list_test)
+list_test.remove(4)
+print(list_test)
+list_test[:] = []
+list_test = [1, 2, 3]
+index = list_test.index(2)
+print(index)
+list_looped = [x * 2 for x in list_test]
+print(list_looped)
+
+matrix = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+]
+transposed_matrix = [[row[i] for row in matrix] for i in range(4)]
+print(transposed_matrix)
+
+transposed = []
+for i in range(4):
+  transposed.append([row[i] for row in matrix])
+  print(transposed)
+
+transposed_second = []
+for i in range(4):
+    transposed_row = []
+    for row in matrix:
+        row = row[i]
+        print(row)
+        transposed_row.append(row[i])
+    transposed_second.append(transposed_row)  
